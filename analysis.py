d4e10f9c17b13b1d796ddea785ef2353c220b71c
Sreeya200 <sreeyakannamala@gmail.com>
2024-11-04 22:06:06 +0300

    add module imports

---
+++ analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
